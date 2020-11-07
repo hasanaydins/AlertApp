f7c6719c08dc9c3b4a4d2779ff90c0185482a33a
Hasan <hasanaydins@gmail.com>
2020-11-08 00:24:27 +0300

    homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,16 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import * as React from 'react';
-import { Button, Text } from 'react-native';
 import DetailHome from './DetailHome';
-import BoxCenter from '../components/BoxCenter';
+import Box from '../components/Box';
+import Button from '../components/Button';
+import Text from '../components/Text';
 import SvgBookmark from '../components/icons/Bookmark';
+import SvgLogoshadow from '../components/icons/Logoshadow';
+import SvgLogopress from '../components/icons/Logopress';
+import SvgMenu from '../components/icons/Menu';
+
+import RadialGradient from 'react-native-radial-gradient';
+import {useState} from 'react';
 
 const HomeStack = createStackNavigator();
 
@@ -11,7 +18,7 @@ function HomeStackScreen() {
   return (
     <HomeStack.Navigator /* headerMode='none'*/>
       <HomeStack.Screen
-        name='Home'
+        name="Home"
         component={HomeScreen}
         options={() => {
           return {
@@ -20,7 +27,7 @@ function HomeStackScreen() {
         }}
       />
       <HomeStack.Screen
-        name='Details'
+        name="Details"
         component={DetailHome}
         options={() => {
           return {
@@ -38,22 +45,42 @@ function HomeStackScreen() {
   );
 }
 
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
+  const [isPlaying, setisPlaying] = useState(false);
+
   return (
-    <BoxCenter>
-      <Text mb={40}>Home screen</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
-            paramID: 86,
-            paramDesc: 'anything you want here',
-            paramHeader: 'Custom profile header',
-          });
-        }}
-      />
-    </BoxCenter>
+    <Box
+      as={RadialGradient}
+      flex={1}
+      colors={['#FF2F54', '#C82731']}
+      stops={[0.1, 1.0]}
+      alignItems="center"
+      justifyContent="center"
+      center={[200, 300]}
+      radius={350}>
+      <Text
+        position="absolute"
+        color="white"
+        top="25%"
+        fontWeight="bold"
+        fontSize={16}>
+        Press alert button to bell.
+      </Text>
+
+      <Button onPress={() => setisPlaying(!isPlaying)}>
+        {isPlaying ? <SvgLogopress /> : <SvgLogoshadow />}
+      </Button>
+
+      <Box bottom="23%" position="absolute" width="75%">
+        <Text color="white" fontWeight="400" fontSize={16} textAlign="center">
+          Or press the menu button to customize the alert sound.
+        </Text>
+      </Box>
+
+      <Button alignSelf='flex-end' mr='12%' position='absolute' bottom='14%'>
+        <SvgMenu color="white" width={34}/>
+      </Button>
+    </Box>
   );
 }
 
