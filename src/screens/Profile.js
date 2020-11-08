90145124e56d446c46fb5ec938c358a0e4701761
Hasan <hasanaydins@gmail.com>
2020-11-08 23:58:40 +0300

    send sms finis

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,63 +3,64 @@ import * as React from 'react';
 import DetailProfile from './DetailProfile';
 import {Platform} from 'react-native';
 
-import {WebView} from 'react-native-webview';
+import LinearGradient from 'react-native-linear-gradient';
 import Box from '../components/Box';
 import Text from '../components/Text';
 import Button from '../components/Button';
 import HTML_FILE from '../../resources/sayfa.html';
 import BoxCenter from '../components/BoxCenter';
+import RadialGradient from 'react-native-radial-gradient';
 const isAndroid = Platform.OS === 'android';
 
 const ProfileStack = createStackNavigator();
 
 function ProfileStackScreen() {
   return (
-    <ProfileStack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <ProfileStack.Screen name="Profile" component={Profile} />
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen
+        name="Profile"
+        component={Profile}
+        options={() => {
+          return {
+            headerShown: false,
+          };
+        }}
+      />
       <ProfileStack.Screen name="Details" component={DetailProfile} />
     </ProfileStack.Navigator>
   );
 }
 
 function Profile({navigation}) {
-  {
-    // Load HTML from a local file
-    if (isAndroid) {
-      return (
-        <Box flex={1}>
-          <WebView
-            style={{flex: 1}}
-            originWhitelist={['*']}
-            source={{ uri: 'file:///android_asset/sayfa.html' }}
-            javaScriptEnabled={true}
-            domStorageEnabled={true}
-          />
-        </Box>
-      );
-    } else {
-      return (
-        <Box flex={1}>
-          <WebView
-            style={{flex: 1}}
-            originWhitelist={['*']}
-            source={HTML_FILE}
-            javaScriptEnabled={true}
-            domStorageEnabled={true}
-          />
-        </Box>
-      );
-    }
-  }
+  return (
+    <Box bg="#F3F3F3" flex={1} px={24} py={24}>
+      <Text fontSize={20} fontWeight="bold">
+        News
+      </Text>
+
+      <Box flexDirection="row" mt={24}>
+        <Button
+          as={LinearGradient}
+          colors={['#FF3E4A', '#CA2D37']}
+          py={10}
+          px={32}
+          borderRadius={6}
+          mr={14}>
+          <Button>
+            <Text color="white" fontWeight="bold">
+              Earthquakes
+            </Text>
+          </Button>
+        </Button>
+        <Button bg="white" py={10} px={32} borderRadius={6} mr={14}>
+          <Text color="black">Fires</Text>
+        </Button>
+        <Button bg="white" py={10} px={32} borderRadius={6} mr={14}>
+          <Text color="black">Floods</Text>
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default ProfileStackScreen;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,33 +2,94 @@ import {createStackNavigator} from '@react-navigation/stack';
 import * as React from 'react';
 import DetailSearch from './DetailSearch';
 import Box from '../components/Box';
-import Text from "../components/Text";
+import Text from '../components/Text';
+import Theme from '../utils/theme';
+
 const SearchStack = createStackNavigator();
 
 function SearchStackScreen() {
   return (
-    <SearchStack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Navigator>
+      <SearchStack.Screen
+        name="Search"
+        component={Search}
+        options={() => {
+          return {
+            headerShown: false,
+          };
+        }}
+      />
       <SearchStack.Screen name="Details" component={DetailSearch} />
     </SearchStack.Navigator>
   );
 }
 
 function Search({props}) {
-
-
   return (
-    <Box style={{flex: 1}}>
-        <Text>sdds</Text>
+    <Box bg="#F3F3F3" flex={1} px={24} py={24}>
+      <Text fontSize={20} fontWeight="bold">
+        Send SMS
+      </Text>
+
+      <Box
+        mt={32}
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center">
+        <Text fontSize={16} fontWeight="bold">
+          Message Content
+        </Text>
+        <Text fontSize={14} color={Theme.colors.oneRed}>
+          Edit
+        </Text>
+      </Box>
+
+      <Box mt={18} borderRadius={8} bg="white" p={16}>
+        <Text fontSize={16}>
+          This is an emergency alert. I need urgent help.
+        </Text>
+        <Text fontSize={16} mt={8}>
+          My Last Location at 23 Oct 2020 13:25: [location]
+        </Text>
+      </Box>
+
+      <Box
+        mt={42}
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center">
+        <Text fontSize={16} fontWeight="bold">
+          Added Numbers
+        </Text>
+        <Text fontSize={14} color={Theme.colors.oneRed}>
+          Add
+        </Text>
+      </Box>
+
+      <Box flexDirection="row" alignItems="center" mt={16}>
+        <Box borderRadius={9999} size={32} bg="#D6D6D6" />
+        <Text fontSize={18} ml={16}>
+          Leila Vaughn
+        </Text>
+      </Box>
+      <Box flexDirection="row" alignItems="center" mt={16}>
+        <Box borderRadius={9999} size={32} bg="#D6D6D6" />
+        <Text fontSize={18} ml={16}>
+          Lina Bullock
+        </Text>
+      </Box>
+      <Box flexDirection="row" alignItems="center" mt={16}>
+        <Box borderRadius={9999} size={32} bg="#D6D6D6" />
+        <Text fontSize={18} ml={16}>
+          Nicholas Jensen
+        </Text>
+      </Box>
+      <Box flexDirection="row" alignItems="center" mt={16}>
+        <Box borderRadius={9999} size={32} bg="#D6D6D6" />
+        <Text fontSize={18} ml={16}>
+          Brother
+        </Text>
+      </Box>
     </Box>
   );
 }
